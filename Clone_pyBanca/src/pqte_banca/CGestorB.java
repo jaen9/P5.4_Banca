118754c12c4866b603ced234164b7af26ad70778
socek <socek@MSI>
2020-05-23 15:48:49 +0200

    He desarrollado la documentación Javadoc de CGestorB

--- Clone_pyBanca/src/pqte_banca/CGestorB.java
+++ Clone_pyBanca/src/pqte_banca/CGestorB.java
@@ -1,12 +1,37 @@
 package pqte_banca;
 import java.util.ArrayList;
 
+/**
+ *  Clase del gestor del banco que puede modificar un cliente o añadir un cliente
+ * @author Sergio Jaen
+ * @version 1.0
+ * @since 1.0
+ */
 public class CGestorB {
     
     //Constructor
+
+    /**
+     * Constructor vacio
+     */
     public CGestorB(){}
     
     //Metodos
+
+    /**
+     * Método que actualiza un cliente que se pasa por parámetro asignándole 
+     * de nuevo todos sus atributos. 
+     * 
+     * @param p1 Cliente
+     * @param nomb Nombre
+     * @param ape1 Apellido1
+     * @param ape2 Apellido2
+     * @param dir Direccion
+     * @param nif Nif
+     * @param tlf Tlf
+     * @param edad Edad
+     * @param cue Cuenta
+     */
     public void modificarPerfil(CCliente p1,
             String nomb,
             String ape1,
@@ -28,6 +53,20 @@ public class CGestorB {
     
     }//modificarPerfil
     
+    /**
+     * Método que crea un nuevo cliente recibiendo por parámetro todos sus
+     * atributos y devuelve el objeto cliente
+     * 
+     * @param nomb Nombre
+     * @param ape1 Apellido1
+     * @param ape2 Apellido2
+     * @param dir Direccion
+     * @param nif Nif
+     * @param tlf Tlf 
+     * @param edad Edad
+     * @param cue Cuenta
+     * @return objeto CCliente creado.
+     */
     public CCliente crearPerfil (
             String nomb,
             String ape1,
